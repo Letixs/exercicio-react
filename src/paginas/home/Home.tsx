d601e1b206fc4154501d846ae3f19098313088c8
Letícia Queiroga <letqueirogaa@outlook.com>
2023-05-12 09:43:14 -0300

    [Feature Leticia - Task : Integração React| 12.05.2023]

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+
+function Home(){
+    return(
+    <>
+        <h1 className='titulo'>Hello World</h1>
+        <img className='img'src="https://png.pngtree.com/thumb_back/fw800/back_pic/04/47/12/785857e15ddbe19.jpg"></img>
+    </>
+    )
+}
+
+export default Home;
